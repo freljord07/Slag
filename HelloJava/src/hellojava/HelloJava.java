de5c98c7b29ea6fbbbdada53b72643386a1cbe8c
unknown <thanhhuynh810840@gmail.com>
2021-06-15 15:58:55 +0700

    Hello

---
+++ HelloJava/src/hellojava/HelloJava.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hellojava;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class     HelloJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Xin chao Java");
+        System.out.println("5+5="+5+5);
+        System.out.println("5+5="+(5+5));
+        //sout tab 
+        //~100% printf("Xin chao Java\n);
+        //F6 là complile
+    }
+    
+}
